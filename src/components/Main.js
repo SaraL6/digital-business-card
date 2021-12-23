a0fe1c11dc15e945d25b7ca68afffbf46b6da235
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2021-12-23 19:44:50 +0100

    Main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,22 @@ import ReactDOM from "react-dom";
 
 export default function Main() {
     return (
-        <h1>main</h1>
+        <main>
+             <div class="about">
+            <h1 className="about--title">About</h1>
+            <p className="about--text">A web developer with a strong interest in projects that require both conceptual and analytical thinking.
+                Fully-committed to designing and developing innovative web-based applications.
+                Always eager to collaborate and learn from others.
+            </p>
+        </div> 
+        <div className="interests">
+        <h1 className="interests--title">Interests</h1>
+            <p className="interests--text">Gamer, mother of three cats, tv show binger, food lover.
+            </p>
+        </div>
+        </main>
+      
+
 
     )
 }
